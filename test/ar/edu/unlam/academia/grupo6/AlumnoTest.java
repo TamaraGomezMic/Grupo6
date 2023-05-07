9229eb82e07df29f1407d621885258652af2ebbd
Tamara Gomez <tamara.gomez.mic@gmail.com>
2023-05-07 00:19:34 -0300

    comienzo proyecto grupo 6

---
+++ test/ar/edu/unlam/academia/grupo6/AlumnoTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.academia.grupo6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AlumnoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/ar/edu/unlam/academia/grupo6/InstructorTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.academia.grupo6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InstructorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
